2c359a9a34c55d0d04d4c7726792b9ab6b521db1
Topdevman <pkpavlo27@yahoo.com>
2017-02-28 02:44:11 -0800

    New Vue project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <img src="./assets/bootsImg/bootsImg@2x.png" class="img1">
+    <img src="./assets/profileImg/profileImg@2x.png" class="img2">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'app'
+    }
+</script>
+
+<style src="./style/my-project.sass"></style>
